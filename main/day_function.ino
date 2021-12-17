36e5e2ab6bc53dfbe54c7a35a606d3310d5d4265
Luca Zammarchi <luca.zammarchi@winwinit.eu>
2021-12-17 19:33:13 +0100

    first version

---
+++ main/day_function.ino
@@ -0,0 +1,53 @@
+
+
+int internalHourSunrise=0;
+void sunrise(long hourOfDay,long period){
+  
+  if(period==0){
+    valueSunrise=0;
+    internalHourSunrise=0;
+  }else{
+    if(internalHourSunrise==0){
+      internalHourSunrise=hourOfDay;
+    }
+    int uno=hourOfDay-internalHourSunrise;
+    uno=period-uno;
+    //Serial.println(hourOfDay);
+    //Serial.println(internalHourSunrise);
+    //Serial.println(uno);
+    valueSunrise+=255/uno;
+    Serial.write("SUNRISE: ");
+    Serial.println(valueSunrise);
+  }
+  
+}
+
+int internalHourSunset=0;
+void sunset(long hourOfDay,long period){
+  if(period==0){
+    valueSunset=0;
+    internalHourSunset=0;
+  }else{  
+    if(internalHourSunset==0){
+      internalHourSunset=hourOfDay;
+    }  
+    int uno=hourOfDay-internalHourSunset;
+    uno=period-uno;
+    Serial.println(uno);
+    valueSunset+=255/uno;
+    Serial.write("SUNSET: ");
+    Serial.println(valueSunset);
+  }
+  
+}
+
+void night(){
+  Serial.println("NIGHT");
+  valueNight=255;
+  valueDay=0;
+}
+void day(){
+  Serial.println("DAY");
+  valueDay=255;
+  valueNight=0;
+}
--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,22 @@
-#define PWM_PIN1 3
-#define PWM_PIN2 5
-#define PWM_PIN3 6
-#define PWM_PIN4 9
+#define PWM_PIN1 3 //Alba
+#define PWM_PIN2 5 //Giorno
+#define PWM_PIN3 6 //Tramonto
+#define PWM_PIN4 9 //Notte
 
-int value = 0;
+#define DAY_HOURS 24
+#define CYCLE_TIMEOUT 300
+#define CYCLE_INCREMENT 600
+
+int cycleCount=0;
+
+int hourOfDay=0;
+int minute=0;
+
 bool inc=true;
+int valueSunrise = 0;
+int valueSunset = 0;
+int valueDay = 0;
+int valueNight = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -14,26 +26,56 @@ void setup() {
   pinMode(PWM_PIN4, OUTPUT);
 }
 
+//ONE CYCLE IS ONE SECONDS
 void loop() {
-  /* analogWrite, come secondo parametro accetta valori da 0 a 
-   * 255. 
-  */
-  analogWrite(PWM_PIN1, value);
-  analogWrite(PWM_PIN2, value);
-  analogWrite(PWM_PIN3, value);
-  analogWrite(PWM_PIN4, value);
-  if(inc)
-    value++;
-  else
-    value--;
+  cycleCount+=CYCLE_INCREMENT;
+  Serial.write("ORA: ");
+  Serial.println(hourOfDay);
 
-  if (value > 255){
-    inc=false;
-    value = 255;
+  if(cycleCount>0 && (cycleCount % 60==0)){
+    minute+=cycleCount/60;
+    cycleCount=0;
   }
-  if(value<0){
-    inc=true;
-    value=00;
+
+  if(minute>0 && minute % 60==0){
+    minute=0;
+    if(hourOfDay==23){
+      hourOfDay=0;
+    }else{
+      hourOfDay++;
+    }
   }
-  delay(20);
+  
+  if(hourOfDay>=0 && hourOfDay<=7){
+    night();
+  }
+  if(hourOfDay>=5 && hourOfDay<=9){
+    //PERIOD 4h
+    sunrise((hourOfDay*60)+minute,300);
+  }
+  if(hourOfDay>9){
+    sunrise(hourOfDay,0);
+  }
+  if(hourOfDay>=8 && hourOfDay<=19){
+    day();
+  }
+  if(hourOfDay>=16 &&hourOfDay<=20){
+    //PERIOD 4h
+    sunset((hourOfDay*60)+minute,300);
+  }
+  if(hourOfDay>=20 && hourOfDay<=24){
+    night();
+  }
+  if(hourOfDay>20){
+    sunset(hourOfDay,0);
+  }
+  
+  
+  
+  analogWrite(PWM_PIN1, valueSunrise);
+  analogWrite(PWM_PIN2, valueDay);
+  analogWrite(PWM_PIN3, valueSunset);
+  analogWrite(PWM_PIN4, valueNight);
+
+  delay(CYCLE_TIMEOUT);
 }
